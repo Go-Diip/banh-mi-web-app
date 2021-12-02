162c9fe2c468db4f222a532fe5535e7a30eb32cc
Bryan Ron <bryan.ron@hotmail.com>
2021-12-01 20:48:34 -0500

    add theme colors

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -13,23 +13,23 @@ const theme = createTheme({
   palette: {
     primary: {
       // contrastText: "rgba(0, 0, 0, 0.87)",
-      main: "#F26B2A",
-      light: "#FFFFFF",
-      dark: "#091118",
+      main: "#c2312c",
+      light: "#f6faf4",
+      dark: "#091211",
     },
     secondary: {
       // contrastText: "rgba(0, 0, 0, 0.87)",
-      main: "#261003",
-      light: "#1F1D2F",
-      dark: "#1F1D2F",
+      main: "#8799ca",
+      light: "#c6747f",
+      dark: "#eba440",
     },
     tertiary: {
       main: "#2C9ED0",
     },
     text: {
-      primary: "#261003",
-      secondary: "#F26B2A",
-      content: "#000000",
+      primary: "#091211",
+      secondary: "#f6faf4",
+      content: "#091211",
       tertiary: "#1A2933",
       disabled: "#10355A",
       hint: "#10355A",

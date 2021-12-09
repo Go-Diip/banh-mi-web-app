e80906f9fc2a45063a06b511f4ed17b68f3c7c6a
Bryan Ron <bryan.ron@hotmail.com>
2021-12-09 00:08:24 -0500

    add menu layout backend without options

--- src/layouts/Menu/menu.component.jsx
+++ src/layouts/Menu/menu.component.jsx
@@ -1,8 +1,19 @@
 import React from "react"
 import * as S from "./menu.styles"
+import { Grid } from "@mui/material"
+import CommonSlider from "../../components/common-slider/common-slider.component"
 
-const Menu = ({}) => {
-  return <></>
+const Menu = ({ images }) => {
+  return (
+    <S.Wrapper>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <CommonSlider images={images} />
+        </Grid>
+        <Grid item xs={12} md={6}></Grid>
+      </Grid>
+    </S.Wrapper>
+  )
 }
 
 export default Menu

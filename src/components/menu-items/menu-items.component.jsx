11dd3e71c2def03a2366f7d35663840ea4370bba
Bryan Ron <bryan.ron@hotmail.com>
2021-12-12 14:09:33 -0500

    feat: add products mapping

--- src/components/menu-items/menu-items.component.jsx
+++ src/components/menu-items/menu-items.component.jsx
@@ -1,11 +1,24 @@
 import React from "react"
 import * as S from "./menu-items.styles"
-import { Container } from "@mui/material"
+import { Container, Grid } from "@mui/material"
 
 const MenuItems = ({ items, title }) => {
+  console.log(items)
   return (
     <Container>
       <h1>{title}</h1>
+      <Grid container spacing={4}>
+        {items.map(({ product }, index) => (
+          <Grid item xs={6}>
+            <S.ProductWrapper>
+              <S.ProductTitle>
+                {product.title} {product.price}
+              </S.ProductTitle>
+              <S.ProductDescription>{product.description}</S.ProductDescription>
+            </S.ProductWrapper>
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   )
 }
---
+++ src/components/menu-items/menu-items.styles.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+export const ProductTitle = styled.span`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 22px;
+  letter-spacing: -0.01em;
+  color: #091211;
+  text-transform: uppercase;
+`
+
+export const ProductDescription = styled.span`
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 22px;
+  letter-spacing: -0.01em;
+  color: #091211;
+`
+
+export const ProductWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1em 0;
+`
+
+export const CategoryWrapper = styled.div`
+  max-width: 600px;
+`

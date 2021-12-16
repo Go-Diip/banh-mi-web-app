2dc2d0148b1e227f2e8cfc68143e9cc5c7dea32a
Bryan Ron <bryan.ron@hotmail.com>
2021-12-15 22:00:07 -0500

    fix: code refactor

--- src/layouts/Menu/menu.component.jsx
+++ src/layouts/Menu/menu.component.jsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react"
 import * as S from "./menu.styles"
-import { Container, Grid, Box, Typography, Modal } from "@mui/material"
-import CommonSlider from "../../components/common-slider/common-slider.component"
-import parse from "html-react-parser"
+import { Container } from "@mui/material"
 import theme from "../../gatsby-theme-material-ui-top-layout/theme"
 import RowGrid from "../../components/row-grid/row-grid.component"
 import CustomButton from "../../components/custom-button/custom-button.component"
-import MenuModal from "../../components/menu-modal/menu-modal.component"
 import { graphql, useStaticQuery } from "gatsby"
-import CustomBgImage from "../../components/custom-bg-image/custom-bg-image.component"
 
 const Menu = ({ image }) => {
   const [toggleState, setToggleState] = useState(0)
@@ -42,17 +38,9 @@ const Menu = ({ image }) => {
               <S.MenuDescription>
                 Para grupos de mas 10 personas, por favor comunicate al
               </S.MenuDescription>
-              <CustomButton className="darkBorder" onClick={handleOpen}>
+              <CustomButton className="darkBorder" href="/menu/">
                 Ver menú
               </CustomButton>
-              <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-              >
-                <MenuModal close={handleClose} />
-              </Modal>
             </S.ContentWrapper>
           </Container>
         </S.RightWrapper>

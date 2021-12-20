73df554f803daf63a6ff6fe43ce1159e5a8f6eea
Rick <rick@aftershock.agency>
2021-12-20 14:58:00 -0500

    feat: wrap reservations logo in link to home

--- src/components/reservations-widget/widget-header/widget-header.component.jsx
+++ src/components/reservations-widget/widget-header/widget-header.component.jsx
@@ -2,11 +2,14 @@ import React from "react"
 import * as S from "./widget-header.styles.jsx"
 import Logo from "../../../assets/logo-red.svg"
 import PoweredBy from "../../../assets/poweredby-dark.svg"
+import GatsbyLink from "gatsby-link"
 
 const WidgetHeader = () => {
   return (
     <S.Wrapper>
-      <Logo />
+      <GatsbyLink to="/">
+        <Logo />
+      </GatsbyLink>
       <PoweredBy />
     </S.Wrapper>
   )

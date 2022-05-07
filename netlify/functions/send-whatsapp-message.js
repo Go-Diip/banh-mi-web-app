c395ffd6f5f6704048e82077c657b02f42cc9948
rick-as <rick@aftershock.agency>
2022-05-07 15:02:33 -0500

    feat: add netlify function

---
+++ netlify/functions/send-whatsapp-message.js
@@ -0,0 +1,35 @@
+exports.handler = function (event, context, callback) {
+  const accountSid = `${process.env.TWILIO_ACCOUNT_SID}` // Your Account SID from www.twilio.com/console
+  const authToken = `${process.env.TWILIO_AUTH_TOKEN}` // Your Auth Token from www.twilio.com/console
+  const { phone, text } = event.body
+
+  // console.log(req.body)
+
+  if (!phone) {
+    console.log("No phone provided. Was not able to send unavailable SMS.")
+    return "No phone provided. Was not able to send unavailable SMS."
+  }
+  const phoneFormatted = `+593${phone.substring(1)}`
+
+  const twilio = require("twilio")
+  const client = new twilio(accountSid, authToken)
+
+  client.messages
+    .create({
+      body: text,
+      to: `whatsapp:${phoneFormatted}`, // Text this number
+      from: "whatsapp:+17869778091", // From a valid Twilio number
+    })
+    .then(message =>
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(message),
+      })
+    )
+    .catch(e =>
+      callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      })
+    )
+}

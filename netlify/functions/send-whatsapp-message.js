c50f1ae2b2605decd393e088f0609bd18502823f
rick-as <rick@aftershock.agency>
2022-05-07 15:20:50 -0500

    fix: bring callback back

--- netlify/functions/send-whatsapp-message.js
+++ netlify/functions/send-whatsapp-message.js
@@ -1,4 +1,4 @@
-exports.handler = (event, context) => {
+exports.handler = (event, context, callback) => {
   const accountSid = `${process.env.TWILIO_ACCOUNT_SID}` // Your Account SID from www.twilio.com/console
   const authToken = `${process.env.TWILIO_AUTH_TOKEN}` // Your Auth Token from www.twilio.com/console
   const { phone, text } = event.body
@@ -20,9 +20,16 @@ exports.handler = (event, context) => {
       to: `whatsapp:${phoneFormatted}`, // Text this number
       from: "whatsapp:+17869778091", // From a valid Twilio number
     })
-    .then(message => ({ statusCode: 200, body: JSON.stringify({ message }) }))
-    .catch(e => ({
-      statusCode: 500,
-      body: JSON.stringify({ error: "Failed fetching data" }),
-    }))
+    .then(message =>
+      callback({
+        statusCode: 200,
+        body: JSON.stringify({ message: "Success" }),
+      })
+    )
+    .catch(e =>
+      callback({
+        statusCode: 500,
+        body: JSON.stringify({ error: "Failed sending message" }),
+      })
+    )
 }

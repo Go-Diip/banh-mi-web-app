361fc4c6059b43059ff2ee993c98160f9232e886
rick-as <rick@aftershock.agency>
2022-05-29 12:50:09 -0500

    feat: move logrocket recording

--- src/pages/reservaciones-reporte.js
+++ src/pages/reservaciones-reporte.js
@@ -3,9 +3,16 @@ import { auth } from "../services/firebase"
 import Layout from "../components/layout"
 import Login from "../components/login/login.component"
 import ReservationsReporter from "../components/reservations-reporter/reservations-reporter.component"
+import LogRocket from "logrocket"
 
 const ReservacionesReporte = () => {
   const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    if (process.env.GATSBY_ACTIVE_ENV === "production") {
+      LogRocket.init("3qav1j/reservations")
+    }
+  }, [])
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
--- src/pages/reservaciones.js
+++ src/pages/reservaciones.js
@@ -2,14 +2,13 @@ import React, { useEffect } from "react"
 
 import Layout from "../components/layout"
 import ReservationsWidget from "../components/reservations-widget/reservations-widget.component"
-import LogRocket from "logrocket"
 
 const Reservations = () => {
-  useEffect(() => {
-    if (process.env.GATSBY_ACTIVE_ENV === "production") {
-      LogRocket.init("3qav1j/reservations")
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (process.env.GATSBY_ACTIVE_ENV === "production") {
+  //     LogRocket.init("3qav1j/reservations")
+  //   }
+  // }, [])
   return (
     <Layout
       seo={{ title: "Reservaciones | Banh Mi " }}

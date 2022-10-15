b48cb503d6888d0fef49e47d35381dae5d80727f
rick-as <rick@aftershock.agency>
2022-10-15 00:20:03 -0500

    fix: gtag

--- src/html.js
+++ src/html.js
@@ -15,17 +15,8 @@ export default function HTML(props) {
         {isProduction && (
           <>
             <script
-              dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','GTM-NNNS44D');`,
-              }}
-            />
-            <script
               async
-              src="https://www.googletagmanager.com/gtag/js?id=G-RT68XJ61D9"
+              src="https://www.googletagmanager.com/gtag/js?id=G-VQT0ZC5SWQ"
             />
             <script
               dangerouslySetInnerHTML={{
@@ -33,7 +24,7 @@ export default function HTML(props) {
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
               
-                gtag('config', 'G-RT68XJ61D9');`,
+                gtag('config', 'G-VQT0ZC5SWQ');`,
               }}
             />
           </>

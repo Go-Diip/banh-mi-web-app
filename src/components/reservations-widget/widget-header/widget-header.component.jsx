434b471d63637c49ed0cfef43a2205a76aff1340
rick-as <rick@aftershock.agency>
2023-01-18 11:12:36 -0500

    fix: times and update packages

--- src/components/reservations-widget/widget-header/widget-header.component.jsx
+++ src/components/reservations-widget/widget-header/widget-header.component.jsx
@@ -1,14 +1,14 @@
 import React from "react"
 import * as S from "./widget-header.styles.jsx"
 import PoweredBy from "../../../assets/poweredby-dark.svg"
-import GatsbyLink from "gatsby-link"
+import { Link } from "gatsby"
 
 const WidgetHeader = () => {
   return (
     <S.Wrapper>
-      <GatsbyLink to="/">
+      <Link to="/">
         <S.CustomLogo />
-      </GatsbyLink>
+      </Link>
       <a
         href="https://www.godiip.com/?utm_source=banhmi+powered+by+link&utm_medium=link+on+reservations+widget+header&utm_campaign=banhmi+powered+by+link"
         target="_blank"

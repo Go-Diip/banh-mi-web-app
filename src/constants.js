0290006f5eb10bcc0e36c8cdb7d8d0694602f6ad
keinelust14 <valladarespaul@gmail.com>
2024-01-05 14:57:52 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -232,7 +232,7 @@ export const ALL_TIME_OPTIONS = [
 ]
 
 // TODO edit this manually everytime the restaurant requires to block times with the required day
-export const BLOCKED_DATES = ["2023-12-22"]
+export const BLOCKED_DATES = ["2024-01-05"]
 // TODO add the times with the corresponding date and area you would like to block, if the area is empty all areas will be blocked
 export const BLOCKED_TIMES = [
   // {
@@ -343,12 +343,12 @@ export const BLOCKED_TIMES = [
     // date: BLOCKED_DATES[0],
     // area: AREAS.RESTAURANTE.value,
   },
-  {
-    value: "21:00",
-    label: "9:00 PM",
-    // date: BLOCKED_DATES[0],
-    // area: AREAS.BAR.value,
-  },
+  // {
+  //   value: "21:00",
+  //   label: "9:00 PM",
+  //   // date: BLOCKED_DATES[0],
+  //   // area: AREAS.BAR.value,
+  // },
   // {
   //   value: "21:15",
   //   // date: BLOCKED_DATES[0],

e1b71379fda93668f6ebba31e2a790470a8b53e3
ricardo-diip <rruiz@godiip.com>
2024-02-04 14:21:30 -0500

    fix: block times today

--- src/constants.js
+++ src/constants.js
@@ -110,7 +110,7 @@ export const EXCEPTIONAL_BAR_OPEN_TIMES = [
 
 // TODO add a date here to prevent from showing this day on the calendar
 // Days restaurant will open regardless of being any day from tuesday to saturday
-export const CLOSE_DATES = ["2024-02-04"]
+export const CLOSE_DATES = ["2023-05-14"]
 
 export const MIN_DATE =
   // TODO uncomment this line to enable the restaurant to be closed on sundays and mondays
@@ -238,141 +238,121 @@ export const ALL_TIME_OPTIONS = [
 ]
 
 // TODO edit this manually everytime the restaurant requires to block times with the required day
-export const BLOCKED_DATES = ["2024-01-05"]
+export const BLOCKED_DATES = ["2024-02-04"]
 // TODO add the times with the corresponding date and area you would like to block, if the area is empty all areas will be blocked
 export const BLOCKED_TIMES = [
+  {
+    value: "12:30",
+    label: "12:30 PM",
+  },
+  {
+    value: "12:45",
+    label: "12:45 PM",
+  },
+  {
+    value: "13:00",
+    label: "1:00 PM",
+  },
+  {
+    value: "13:15",
+    label: "1:15 PM",
+  },
+  {
+    value: "13:30",
+    label: "1:30 PM",
+  },
+  {
+    value: "13:45",
+    label: "1:45 PM",
+  },
+  {
+    value: "14:00",
+    label: "2:00 PM",
+  },
+  {
+    value: "14:15",
+    label: "2:15 PM",
+  },
+  {
+    value: "14:30",
+    label: "2:30 PM",
+  },
+  {
+    value: "14:45",
+    label: "2:45 PM",
+  },
+  {
+    value: "15:00",
+    label: "3:00 PM",
+  },
   // {
-  //   value: "12:30",
-  //   label: "12:30 PM",
-  //   area: AREAS.BAR.value,
+  //   value: "18:00",
+  //   label: "6:00 PM",
   // },
   // {
-  //   value: "12:45",
-  //   label: "12:45 PM",
-  //   area: AREAS.BAR.value,
+  //   value: "18:15",
+  //   label: "6:15 PM",
   // },
   // {
-  //   value: "13:00",
-  //   label: "1:00 PM",
-  //   area: AREAS.BAR.value,
+  //   value: "18:30",
+  //   label: "6:30 PM",
   // },
   // {
-  //   value: "13:15",
-  //   label: "1:15 PM",
-  //   area: AREAS.BAR.value,
+  //   value: "18:45",
+  //   label: "6:45 PM",
   // },
-  // {
-  //   value: "13:30",
-  //   label: "1:30 PM",
-  //   area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "13:45",
-  //   label: "1:45 PM",
-  //   area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "14:00",
-  //   label: "2:00 PM",
-  //   area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "14:15",
-  //   label: "2:15 PM",
-  //   area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "14:30",
-  //   label: "2:30 PM",
-  //   area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "14:45",
-  //   label: "2:45 PM",
-  //   area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "15:00",
-  //   label: "3:00 PM",
-  //   area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "17:00",
-  //   label: "5:00 PM",
-  //   area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "19:00",
-  //   label: "7:00 PM",
-  //   area: AREAS.BAR.value,
-  //   // date: BLOCKED_DATES[0],
-  // },
-  // {
-  //   value: "19:15",
-  //   label: "7:15 PM",
-  //   // date: BLOCKED_DATES[0],
-  //   // area: AREAS.RESTAURANTE.value,
-  // },
+  {
+    value: "19:00",
+    label: "7:00 PM",
+  },
+  {
+    value: "19:15",
+    label: "7:15 PM",
+  },
   {
     value: "19:30",
     label: "7:30 PM",
-    // date: BLOCKED_DATES[0],
-    // area: AREAS.RESTAURANTE.value,
   },
-  {
-    value: "20:00",
-    label: "8:00 PM",
-    // date: BLOCKED_DATES[0],
-    // area: AREAS.RESTAURANTE.value,
-  },
+  // {
+  //   value: "20:00",
+  //   label: "8:00 PM",
+  // },
   {
     value: "20:15",
     label: "8:15 PM",
-    // date: BLOCKED_DATES[0],
-    // area: AREAS.RESTAURANTE.value,
-  },
-  {
-    value: "20:45",
-    label: "8:45 PM",
-    // date: BLOCKED_DATES[0],
-    // area: AREAS.RESTAURANTE.value,
   },
   {
     value: "20:30",
     label: "8:30 PM",
-    // date: BLOCKED_DATES[0],
-    // area: AREAS.RESTAURANTE.value,
   },
   {
     value: "20:45",
     label: "8:45 PM",
-    // date: BLOCKED_DATES[0],
-    // area: AREAS.RESTAURANTE.value,
   },
-  // {
-  //   value: "21:00",
-  //   label: "9:00 PM",
-  //   // date: BLOCKED_DATES[0],
-  //   // area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "21:15",
-  //   // date: BLOCKED_DATES[0],
-  //   // label: "9:15 PM",
-  //   // area: AREAS.BAR.value,
-  // },
-  // {
-  //   value: "21:30",
-  //   label: "9:30 PM",
-  //   // date: BLOCKED_DATES[0],
-  //   // area: AREAS.RESTAURANTE.value,
-  // },
-  // {
-  //   value: "21:45",
-  //   label: "9:45 PM",
-  //   // date: BLOCKED_DATES[0],
-  //   // area: AREAS.RESTAURANTE.value,
-  // },
+  {
+    value: "21:00",
+    label: "9:00 PM",
+  },
+  {
+    value: "21:15",
+    label: "9:15 PM",
+  },
+  {
+    value: "21:30",
+    label: "9:30 PM",
+  },
+  {
+    value: "21:45",
+    label: "9:45 PM",
+  },
+  {
+    value: "22:00",
+    label: "10:00 PM",
+  },
+  {
+    value: "22:30",
+    label: "10:30 PM",
+  },
   // {
   //   value: "22:00",
   //   label: "10:00 PM",

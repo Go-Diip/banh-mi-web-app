2a11772e54a083f34146439f6efb41bc6e47a7d3
ricardo-diip <rruiz@godiip.com>
2024-02-06 14:54:02 -0500

    feat: block feb 14th hours

--- src/constants.js
+++ src/constants.js
@@ -238,53 +238,53 @@ export const ALL_TIME_OPTIONS = [
 ]
 
 // TODO edit this manually everytime the restaurant requires to block times with the required day
-export const BLOCKED_DATES = ["2024-02-04"]
+export const BLOCKED_DATES = ["2024-02-14"]
 // TODO add the times with the corresponding date and area you would like to block, if the area is empty all areas will be blocked
 export const BLOCKED_TIMES = [
-  {
-    value: "12:30",
-    label: "12:30 PM",
-  },
-  {
-    value: "12:45",
-    label: "12:45 PM",
-  },
-  {
-    value: "13:00",
-    label: "1:00 PM",
-  },
-  {
-    value: "13:15",
-    label: "1:15 PM",
-  },
-  {
-    value: "13:30",
-    label: "1:30 PM",
-  },
-  {
-    value: "13:45",
-    label: "1:45 PM",
-  },
-  {
-    value: "14:00",
-    label: "2:00 PM",
-  },
-  {
-    value: "14:15",
-    label: "2:15 PM",
-  },
-  {
-    value: "14:30",
-    label: "2:30 PM",
-  },
-  {
-    value: "14:45",
-    label: "2:45 PM",
-  },
-  {
-    value: "15:00",
-    label: "3:00 PM",
-  },
+  // {
+  //   value: "12:30",
+  //   label: "12:30 PM",
+  // },
+  // {
+  //   value: "12:45",
+  //   label: "12:45 PM",
+  // },
+  // {
+  //   value: "13:00",
+  //   label: "1:00 PM",
+  // },
+  // {
+  //   value: "13:15",
+  //   label: "1:15 PM",
+  // },
+  // {
+  //   value: "13:30",
+  //   label: "1:30 PM",
+  // },
+  // {
+  //   value: "13:45",
+  //   label: "1:45 PM",
+  // },
+  // {
+  //   value: "14:00",
+  //   label: "2:00 PM",
+  // },
+  // {
+  //   value: "14:15",
+  //   label: "2:15 PM",
+  // },
+  // {
+  //   value: "14:30",
+  //   label: "2:30 PM",
+  // },
+  // {
+  //   value: "14:45",
+  //   label: "2:45 PM",
+  // },
+  // {
+  //   value: "15:00",
+  //   label: "3:00 PM",
+  // },
   // {
   //   value: "18:00",
   //   label: "6:00 PM",
@@ -304,14 +304,17 @@ export const BLOCKED_TIMES = [
   {
     value: "19:00",
     label: "7:00 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "19:15",
     label: "7:15 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "19:30",
     label: "7:30 PM",
+    area: AREAS.BAR.value,
   },
   // {
   //   value: "20:00",
@@ -320,22 +323,27 @@ export const BLOCKED_TIMES = [
   {
     value: "20:15",
     label: "8:15 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "20:30",
     label: "8:30 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "20:45",
     label: "8:45 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "21:00",
     label: "9:00 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "21:15",
     label: "9:15 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "21:30",
@@ -344,14 +352,17 @@ export const BLOCKED_TIMES = [
   {
     value: "21:45",
     label: "9:45 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "22:00",
     label: "10:00 PM",
+    area: AREAS.BAR.value,
   },
   {
     value: "22:30",
     label: "10:30 PM",
+    area: AREAS.BAR.value,
   },
   // {
   //   value: "22:00",
